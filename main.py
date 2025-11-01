3e1aa7b89a30c56721d2d58bdf48680a130a603d
MarcoEsquivel <marco.esquivel@cimat.mx>
2025-11-01 16:00:31 -0600

    Define Button class

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+import sys
+sys.path.insert(1, './src')
 import pygame
+import screenTools
 
-# Colors
-SCREEN_COLOR   = (255, 255, 255)  # White
-TOOLBAR_COLOR  = (160, 160, 160)  # Gray
-ROBOT_COLOR    = (0, 0, 255)      # Blue
-GOAL_COLOR     = (255, 0, 0)      # Red
-OBSTACLE_COLOR = (0, 0, 0)        # Black
-PATH_COLOR     = (153, 255, 255)  # Light blue
-MESSAGE_COLOR  = (0, 0, 0)        # Black
-WARNING_COLOR  = (255, 0, 0)      # Red
+
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((900, 600))
+
+    # Colors
+    SCREEN_COLOR         = (255, 255, 255)  # White
+    TOOLBAR_COLOR        = (160, 160, 160)  # Gray
+    BUTTON_COLOR         = (192, 192, 192)
+    BUTTON_PRESSED_COLOR = (128, 128, 128)
+    ROBOT_COLOR          = (0, 0, 255)      # Blue
+    GOAL_COLOR           = (255, 0, 0)      # Red
+    OBSTACLE_COLOR       = (0, 0, 0)        # Black
+    PATH_COLOR           = (153, 255, 255)  # Light blue
+    MESSAGE_COLOR        = (0, 0, 0)        # Black
+    WARNING_COLOR        = (255, 0, 0)      # Red
+
+    # Buttons
+    DRAW_OBSTACLE_BUTTON = screenTools.Button(10, 10, 100, 200, 'Draw obstacle', BUTTON_COLOR, BUTTON_PRESSED_COLOR)
+    screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Bug Algorithms")
 
     running = True
@@ -20,10 +30,14 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                DRAW_OBSTACLE_BUTTON.handle_event(event)
 
         screen.fill(SCREEN_COLOR)  # Fill the screen with white
         toolsBar = pygame.Rect(0, 0, 200, 600)
         pygame.draw.rect(screen, TOOLBAR_COLOR, toolsBar, width=0)
+
+        DRAW_OBSTACLE_BUTTON.draw(screen)
 
         pygame.display.flip()    # Update the display
 
--- src/screenTools.py
+++ src/screenTools.py
@@ -1,6 +1,34 @@
 import pygame
 
 class Button:
+    def __init__(self, x, y, width, height, text, color, pressedColor, wasPressed=False):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = color
+        self.pressedColor = pressedColor
+        self.currentColor = color
+        self.font = pygame.font.SysFont("Arial", 24)
+        self.wasPressed = wasPressed
 
-class Visualizer:
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.currentColor, self.rect)
+
+        text_surface = self.font.render(self.text, True, (255, 255, 255)) # White text
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        surface.blit(text_surface, text_rect)
+
+    def handle_event(self, event):
+        if self.rect.collidepoint(event.pos):
+            self.wasPressed = True
+            self.currentColor = self.pressedColor
+
+    def reset(self):
+        self.wasPressed = False
+        self.currentColor = self.color
+
+
+
+#class Visualizer:
+
+#class SlideBar:
     
