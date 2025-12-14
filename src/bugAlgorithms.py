33a54a1b8c74174f6822201528264e008cf682bd
MarcoEsquivel <marcobasaldua1@gmail.com>
2025-12-14 17:32:06 -0600

    Include delta for min distance

--- src/bugAlgorithms.py
+++ src/bugAlgorithms.py
@@ -12,12 +12,13 @@ def bug1(screen, obstacleColor, robot, goal):
 
         if collision:
             mustFollowObstacle = True
+            # Check if obstacle has been encircled
             if len(robot.hitPoints) > robot.minHitPoints:
                 if dist(robot.hitPoints[-1][0], robot.hitPoints[0][0]) < 3.0:
                     robot.obsEncircled = True
             
             if robot.obsEncircled:
-                if dist(robot.pos, goal.pos) <= (robot.minDist2Goal + 1):
+                if dist(robot.pos, goal.pos) <= (robot.minDist2Goal + 2):
                     mustFollowObstacle = False
 
             if mustFollowObstacle:
